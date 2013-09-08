94311cde6226027108ed7d24aa427c198b0d5ba8
opagani <paganio@yahoo.com>
2013-09-08 14:42:44 -0700

    about and contact

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,6 +3,8 @@ class VisitorsController < ApplicationController
   def new
 
     @owner = Owner.new
+    flash[:notice] = 'Welcome!'
+    flash[:alert] = 'My birthday is soon.'
     render 'visitors/new'
   end
 
