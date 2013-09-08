9c3be877bd4dfaab031cca21594f6d7edde046e7
opagani <paganio@yahoo.com>
2013-09-08 15:52:41 -0700

    save data to a spreadsheet

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,8 +3,6 @@ class VisitorsController < ApplicationController
   def new
 
     @owner = Owner.new
-    flash[:notice] = 'Welcome!'
-    flash[:alert] = 'My birthday is soon.'
     render 'visitors/new'
   end
 
